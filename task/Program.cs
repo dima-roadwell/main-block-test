53fd609bcd89f6dc22fafa6633d232c01ae59cb3
dima-roadwell <aloun123@mail.ru>
2023-07-25 17:01:09 +0300

    add method 'SetArraySize'

--- task/Program.cs
+++ task/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int SetArraySize()
+{
+    System.Console.Write("Введите длину массива:");
+    int arrSize = Convert.ToInt32(Console.ReadLine());
+
+    return arrSize;
+}
+
+int countItems = SetArraySize();
